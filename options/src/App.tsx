0a3d25c52aa2784fcb690b49e8d845b1ea503262
Shubham Singh <shubh622005@gmail.com>
2024-09-14 19:20:39 +0530

    feat(options): added routing with router-dom

--- options/src/App.tsx
+++ options/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { HomeComponent } from './pages/home';
+import { ImportComponent } from './pages/import';
+import { CreateNewComponent } from './pages/createNewWallet';
+import { Pages } from './@types';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [page, setPage] = useState<Pages>(Pages.MAIN);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const renderPage = () => {
+    switch (page) {
+      case Pages.NEW:
+        return <CreateNewComponent />;
+      case Pages.IMPORT:
+        return <ImportComponent />;
+      default:
+        return <HomeComponent setPage={setPage} />;
+    }
+  };
+
+  return renderPage();
 }
-
-export default App
